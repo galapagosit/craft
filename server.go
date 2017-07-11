e580aa6591dbad4bd4ffd940921abc06bdb3b123
Daisuke Hashimoto <galapagosit@gmail.com>
2017-07-11 12:16:14 +0900

    example

--- server.go
+++ server.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
+	"html/template"
+	"io"
+	"net/http"
 )
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	e := echo.New()
+
+	e.Logger.SetLevel(log.DEBUG)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e.Renderer = t
+
+	e.Static("/static", "assets")
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.Render(http.StatusOK, "index", "World")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
