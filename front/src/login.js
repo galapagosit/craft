7b3e7d87f2401894df4d1380b6915582eea4253a
Daisuke Hashimoto <galapagosit@gmail.com>
2017-08-12 16:52:18 +0900

    delete front

--- front/src/login.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import Login from './Login.vue'
-
-new Vue({
-  el: '#app',
-  render: h => h(Login)
-})
--- front/src/register.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import Register from './Register.vue'
-
-new Vue({
-  el: '#app',
-  render: h => h(Register)
-})
