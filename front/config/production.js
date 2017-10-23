e08229967cfc7267d735788328911b912d8c35b1
Daisuke Hashimoto <galapagosit@gmail.com>
2017-10-23 10:32:33 +0900

    api

--- front/config/production.js
+++ front/config/production.js
@@ -1,3 +1,11 @@
 module.exports = {
-  BASE_URL : 'http://localhost:1323'
+  BASE_URL: 'https://api.craft.popbits.co.jp',
+  FETCH_BASE_PARAMS: {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    mode: 'cors',
+    credentials: 'include'
+  },
 }
